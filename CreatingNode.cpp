464a1c3e50b73021c2d4d67ef536b4f2a9fd106a
Sameer-s7 <sameergoutam794@gmail.com>
2026-05-28 21:28:18 +0530

    updated files

---
+++ CreatingNode.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+using namespace std;
+class Node
+{  
+    public:
+    int data;
+    Node* next;
+    
+    
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+int main(){
+    int arr[5] = {10,12,15,20,11};
+    Node *Head  = NULL , *Tail = NULL; 
+
+    // create first node 
+    for(int i = 0;i<5;i++){
+        if(!Head){// first node creation
+            Head = new Node(arr[0]);
+            Tail = Head;
+        }
+
+    else{
+        // other node creation by adding the element in the last 
+        Tail->next = new Node(arr[i]);
+        Tail = Tail->next;
+    }
+    }
+// initially ---10 
+// insert 20 Tail->next == 20
+// 10 -> 20 ->30
+
+// traverse the linked list 
+    Tail = Head;
+
+    while(Tail!=NULL){
+        cout<<Tail->data<<" ";
+        Tail = Tail->next;
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// // creating Node
+// class Node{
+//     public:
+//     int data;
+//     Node *next;
+//     Node(int value)
+//     {
+//         data = value;
+//         next = NULL;
+//     }
+// };
+// int main(){
+//     Node A1(4); // this is not dynamic way of creating object 
+//     /// object creation with dynamic way'
+//     Node *Head; 
+//     Head = new Node(4);
+//     cout<<Head->data<<endl;
+//     cout<<Head->next<<endl;
+
+// }
